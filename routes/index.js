1d4992a9f22b312f8121615194b21f9364c95e03
KryterX <49222266+KryterX@users.noreply.github.com>
2022-11-19 21:47:10 -0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', require ('./swagger'))
-router.use('/contacts', require('./contacts'))
+router.use('/', require('./swagger'));
+router.use('/user', require('./user'));
+router.use('/theme', require('./theme'));
 
 module.exports = router;
